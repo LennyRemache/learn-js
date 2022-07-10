b9f22ee7b467905e71afebf1a31777cf762ab4f3
LennyRemache <lenny.j.remache@gmail.com>
2022-07-10 15:33:14 -0400

    Hello World and variables

---
+++ lesson-01.js
@@ -0,0 +1,12 @@
+/* Lesson 1: Hello World and Variables */
+
+console.log("Hello World");
+
+let num = 2 + 2; //expression stored in variable num
+console.log(num);
+
+let userName = "Lenny";
+console.log(userName);
+
+num = 10;
+console.log(num);
