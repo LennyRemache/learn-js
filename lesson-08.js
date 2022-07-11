25be91c035aedb0ebca95c8aee61d9ff60dd2181
LennyRemache <lenny.j.remache@gmail.com>
2022-07-11 19:26:52 -0400

    Conditional Statements

---
+++ lesson-08.js
@@ -0,0 +1,11 @@
+/* Lesson 8: Conditional Statements */
+
+if (3 % 2 === 0 && 3 % 3 === 0) {
+    console.log("Even");
+}
+else if (6 % 2 === 0) {
+    console.log("Even 6")
+}
+else {
+    console.log("Odd");
+}
