5152f04adfd247cb6385160f0b649e13975b97fe
LennyRemache <lenny.j.remache@gmail.com>
2022-07-11 19:39:10 -0400

    Ternary Operator

---
+++ lesson-09.js
@@ -0,0 +1,7 @@
+/* Lesson 9: Ternary Operator */
+
+let num = 3;
+num % 2 === 0 ? console.log('Even') : console.log('Odd');
+
+let num2 = 6;
+num2 % 2 === 0 ? console.log('Even') : console.log('Odd');
