3516053fab18fa85900e646d5a212101eebeb6be
LennyRemache <lenny.j.remache@gmail.com>
2022-07-12 19:55:57 -0400

    Template Literal

---
+++ lesson-11.js
@@ -0,0 +1,8 @@
+/* Lesson 11: Template Literal */
+
+let num1 = 7;
+let num2 = 4;
+let result = num1 + num2;
+
+// console.log("The addition of " + num1 + " and " + num2 + " is result");
+console.log(`The addition of ${num1} and ${num2} is result`);
