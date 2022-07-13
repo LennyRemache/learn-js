4b80755cd63ed09e7a6bec00e4dbc5550fd7fc98
LennyRemache <lenny.j.remache@gmail.com>
2022-07-13 18:46:29 -0400

    Methods

---
+++ lesson-19.js
@@ -0,0 +1,14 @@
+/* Lesson 19: Methods */
+
+let laptop = {
+    cpu: "i9",
+    ram: 16,
+    brand: "HP",
+
+    greet: () => {
+        console.log("Hello World!");
+    }
+}
+
+// laptop's greet method
+laptop.greet();
